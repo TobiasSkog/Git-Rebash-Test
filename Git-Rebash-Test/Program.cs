c2331a18f081d8b92c2461b454e36dac52f6150e
Tobias Skog <t_ott_e@hotmail.com>
2024-10-19 21:14:27 +0200

    updated how we write hello wold

--- Git-Rebash-Test/Program.cs
+++ Git-Rebash-Test/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello\nWorld!");
         Apa.PrintFoo();
         Get.PrintBar();
     }
